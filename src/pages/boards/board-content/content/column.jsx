e49ee565961b38b1a655c58b0f2f1df203e891fc
Suresh Konar <konarssuresh@gmail.com>
2026-01-12 22:41:36 +0530

    feat: move tasks across different columns UI part

--- src/pages/boards/board-content/content/column.jsx
+++ src/pages/boards/board-content/content/column.jsx
@@ -1,6 +1,12 @@
 import { clsx } from "clsx";
+import { useEffect, useRef, useState } from "react";
 import Task from "./task";
+import { dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+
 const Column = ({ column }) => {
+  const columnsContainerRef = useRef();
+  const [isElementBeingDragged, setIsElementBeingDragged] = useState(false);
+
   const columnContainer = clsx({
     "w-70 flex-shrink-0": true,
     // flex container styles
@@ -12,18 +18,56 @@ const Column = ({ column }) => {
   });
 
   const tasksContainer = clsx({
-    "flex flex-col gap-4 overflow-y-auto": true,
+    "flex flex-col gap-4 overflow-y-auto min-h-[90%]": true,
+    "bg-blue-400/25": isElementBeingDragged,
   });
+
+  const previewContainerClasses = clsx({
+    "text-lg text-grey-400": true,
+  });
+
+  useEffect(() => {
+    const element = columnsContainerRef.current;
+
+    if (!element) {
+      return;
+    }
+
+    const cleanup = dropTargetForElements({
+      element,
+      // canDrop
+      onDrop: ({ source }) => {
+        console.log(source);
+        setIsElementBeingDragged(false);
+      },
+      onDragEnter: () => {
+        setIsElementBeingDragged(true);
+      },
+      onDragLeave: () => {
+        setIsElementBeingDragged(false);
+      },
+    });
+
+    return cleanup;
+  }, [column]);
+
+  console.log(`isBeingDraggedInto - ` + isElementBeingDragged + column?.name);
 
   return (
     <div className={columnContainer}>
       <h4 className={columnTitleClasses}>
         {column.name.toUpperCase()} {`(${column.tasks.length})`}
       </h4>
-      <div className={tasksContainer}>
-        {column.tasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))}
+      <div className={tasksContainer} ref={columnsContainerRef}>
+        {!isElementBeingDragged ? (
+          <>
+            {column.tasks.map((task) => (
+              <Task key={task.id} task={task} />
+            ))}
+          </>
+        ) : (
+          <div className={previewContainerClasses}>Move to {column?.name}</div>
+        )}
       </div>
     </div>
   );
--- src/pages/boards/board-content/content/task.jsx
+++ src/pages/boards/board-content/content/task.jsx
@@ -1,8 +1,14 @@
+import { useState, useEffect, useRef } from "react";
 import clsx from "clsx";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 
 const Task = ({ task }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const taskContainerRef = useRef(null);
   const taskContainer = clsx({
-    "bg-white rounded-md p-4 flex flex-col gap-2 shadow-md": true,
+    "rounded-md p-4 flex flex-col gap-2 shadow-md": true,
+    "opacity-20": isDragging,
+    "bg-white": !isDragging,
   });
 
   const taskTitleClasses = clsx({
@@ -13,13 +19,35 @@ const Task = ({ task }) => {
     "text-md text-grey-400": true,
   });
 
+  useEffect(() => {
+    const element = taskContainerRef.current;
+    if (!element) {
+      return;
+    }
+
+    const cleanup = draggable({
+      element,
+      getInitialData() {
+        return task;
+      },
+      onDragStart: () => {
+        setIsDragging(true);
+      },
+      onDrop: () => {
+        setIsDragging(false);
+      },
+    });
+
+    return cleanup;
+  }, [task]);
+
   const subTasks = task?.subtasks || [];
   const completedSubTasks = subTasks.reduce((count, subTask) => {
     return subTask.isCompleted ? count + 1 : count;
   }, 0);
 
   return (
-    <div className={taskContainer}>
+    <div className={taskContainer} ref={taskContainerRef}>
       <h6 className={taskTitleClasses}>{task.title}</h6>
       <p className={completedSubTasksClasses}>
         {completedSubTasks} of {subTasks?.length} subtasks
